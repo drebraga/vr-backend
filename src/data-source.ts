0cdb897cc05a1c3d9893053dc70feb092d31a9be
drebraga <dre.braga@gmail.com>
2023-09-11 01:27:54 -0300

    config: migration config to update database

--- src/data-source.ts
+++ src/data-source.ts
@@ -14,6 +14,6 @@ export const AppDataSource = new DataSource({
   synchronize: true,
   logging: false,
   entities: [Produto, produtoloja, loja],
-  migrations: [],
+  migrations: [__dirname + '/migration/**/*.ts'],
   subscribers: [],
 });
